a0834236db194911fb1e600ff09a5483668974af
Duncan Sparks <duncan.sprks@gmail.com>
2020-04-06 19:06:50 -0700

    Add foundation for attacks

--- src/Player/Player.cpp
+++ src/Player/Player.cpp
@@ -21,6 +21,10 @@ void Player::_register_methods()
 	REGISTER_METHOD(Player, _process);
 	REGISTER_METHOD(Player, _physics_process);
 	REGISTER_METHOD(Player, _input);
+
+	REGISTER_METHOD(Player, set_state);
+	REGISTER_METHOD(Player, stop);
+
 	REGISTER_METHOD(Player, _on_HitboxGround_body_entered);
 	REGISTER_METHOD(Player, _on_HitboxGround_body_exited);
 	REGISTER_METHOD(Player, _on_HitboxCeiling_body_entered);
@@ -41,7 +45,8 @@ void Player::_ready()
 
 	camera = GET_NODE(Camera, "CameraPivot/Camera");
 	camera_pivot = GET_NODE(Position3D, "CameraPivot");
-	model = GET_NODE(MeshInstance, "MeshInstance");
+	model = GET_NODE(MeshInstance, "Model");
+	anim_player = GET_NODE(AnimationPlayer, "AnimationPlayer"); // TODO: Change to an AnimationTree when we get that system in place
 }
 
 
@@ -61,20 +66,35 @@ void Player::_input(InputEvent* event)
 
 void Player::_process(float delta)
 {
-	move_direction = Vector3{0, 0, 0};
-	const Transform camera_xform = camera->get_global_transform();
+	if (state != State::Attack) {
+		move_direction = Vector3{ 0, 0, 0 };
+		const Transform camera_xform = camera->get_global_transform();
 
-	move_direction += -camera_xform.basis.z * inp->get_action_strength("move_forward");
-	move_direction += camera_xform.basis.z * inp->get_action_strength("move_back");
-	move_direction += -camera_xform.basis.x * inp->get_action_strength("move_left");
-	move_direction += camera_xform.basis.x * inp->get_action_strength("move_right");
+		move_direction += -camera_xform.basis.z * inp->get_action_strength("move_forward");
+		move_direction += camera_xform.basis.z * inp->get_action_strength("move_back");
+		move_direction += -camera_xform.basis.x * inp->get_action_strength("move_left");
+		move_direction += camera_xform.basis.x * inp->get_action_strength("move_right");
+	}
+	
+	if (inp->is_action_just_pressed("move_jump") && (state == State::Ground || state == State::Air)) {
+		if (jumps < 2)
+			jump();
+	}
 
-	if (inp->is_action_just_pressed("move_jump")) {
-		if (on_ground || jumps < 2) {
-			y_velocity = jump_force;
-			jumps++;
-			on_ground = false;
-		}
+	if (inp->is_action_just_pressed("attack_claw") && state == State::Ground) {
+		stop();
+		state = State::Attack;
+		anim_player->play("Attack");
+	}
+
+	if (inp->is_action_just_pressed("attack_pounce") && state == State::Ground) {
+		stop();
+		state = State::Attack;
+
+		float rot = model->get_rotation().y;
+		move_direction = -Vector3{ std::sin(rot), 0, std::cos(rot) } * 2.0f;
+
+		anim_player->play("Pounce");
 	}
 	
 	if (inp->is_action_just_pressed("sys_quit"))
@@ -97,7 +117,7 @@ void Player::_physics_process(float delta)
 	if (is_moving()) {
 		target_rotation = std::atan2(move_direction.x, move_direction.z) + Mathf::deg2rad(180);
 		Vector3 rot = model->get_rotation();
-		rot.y = Mathf::lerp_delta(rot.y, rot.y + get_closest_angle(fmod(abs(rot.y), 2 * Mathf::Pi), target_rotation, rot.y < 0? true : false), 0.0005f, delta);
+		rot.y = Mathf::lerp_delta(rot.y, rot.y + get_closest_angle(fmod(abs(rot.y), 2 * Mathf::Pi), target_rotation, rot.y < 0), 0.0005f, delta);
 		model->set_rotation(rot);
 	}
 
@@ -124,11 +144,33 @@ inline float Player::get_closest_angle(float current, float target, bool flip) {
 }
 
 
+void Player::jump()
+{
+	y_velocity = jump_force;
+	jumps++;
+	on_ground = false;
+	state = State::Air;
+}
+
+
 void Player::land()
 {
 	y_velocity = 0;
 	jumps = 0;
 	on_ground = true;
+	state = State::Ground;
+}
+
+
+void Player::stop()
+{
+	move_direction = Vector3{ 0, 0, 0 };
+}
+
+
+void Player::set_state(int value)
+{
+	state = static_cast<State>(value);
 }
 
 
@@ -145,6 +187,7 @@ void Player::_on_HitboxGround_body_exited(Node* body)
 	if (body->is_in_group("Ground")) {
 		jumps = 1;
 		on_ground = false;
+		state = State::Air;
 	}
 }
 
@@ -154,5 +197,6 @@ void Player::_on_HitboxCeiling_body_entered(Node* body)
 	if (body->is_in_group("Ground") && y_velocity > 0) {
 		y_velocity = 0;
 		on_ground = false;
+		state = State::Air;
 	}
 }
--- src/Player/Player.hpp
+++ src/Player/Player.hpp
@@ -7,12 +7,19 @@
 #include <Position3D.hpp>
 #include <MeshInstance.hpp>
 #include <InputEvent.hpp>
-#include <Timer.hpp>
+#include <AnimationPlayer.hpp>
 
 namespace godot {
 
 	class Player : public KinematicBody {
 		GODOT_CLASS(Player, KinematicBody)
+
+	public:
+		enum class State {
+			Ground,
+			Air,
+			Attack
+		};
 
 	private:
 		float speed = 4.0f;
@@ -28,10 +35,12 @@ namespace godot {
 		float target_rotation = 0;
 		float camera_joy_value = 0;
 
-		// Children
+		State state = State::Ground;
+
 		Camera* camera;
 		Position3D* camera_pivot;
 		MeshInstance* model;
+		AnimationPlayer* anim_player; // TODO: Change to an AnimationTree when we get that system in place
 
 	public:
 		static void _register_methods();
@@ -50,7 +59,11 @@ namespace godot {
 	private:
 		inline bool is_moving();
 		inline float get_closest_angle(float current, float target, bool flip);
+		void jump();
 		void land();
+		void stop();
+
+		void set_state(int value);
 
 		void _on_HitboxGround_body_entered(Node* body);
 		void _on_HitboxGround_body_exited(Node* body);
